f6910b8badcaee62161e8056508fc9c5beee766e
Kemelite1 <osikemelite42@gmail.com>
2024-01-31 15:27:18 +0100

    making corrections on task_2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
-  performance: {
-    maxAssetSize: 1000000,
-  },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(?:svg|gif|png|jpg|jpeg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            }
-          }
-        ]
+        test: /\.{png|svg|jpg|jpeg|gif}$/i,
+        type: 'asset/resource'
       },
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+    ],
+  },
 };
