6ddc55f161b0d82fccb5c4bb665ce0dfe02fd98c
Kemelite1 <osikemelite42@gmail.com>
2024-03-05 18:22:34 +0100

    created reducer for notifications

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionCreators.test.js
@@ -0,0 +1,15 @@
+import { MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters } from './notificationActionTypes';
+import { markAsAread, setNotificationFilter } from './notificationActionCreators';
+
+describe('tests for notification actions', () => {
+	it('should create right action for mark as read', () => {
+		expect(markAsRead(1)).toEqual({ type: MARK_AS_READ, index: 1 });
+	});
+
+	it('should create right action for notification filter', () => {
+		expect(setNotificationFilter(NotificationTypeFilters['DEFAULT'])).toEqual({
+			type: SET_TYPE_FILTER,
+			filter: 'DEFAULT',
+		});
+	});
+});
